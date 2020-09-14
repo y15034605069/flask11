603c69dea44d7d391a2e16270b5068f526ee62cc
y15034605069 <jhmyydd123123@qq.com>
2020-09-14 19:15:43 +0800

    v1

--- manage.py
+++ manage.py
@@ -11,6 +11,13 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/add_user')
+def add_user():
+    return Response('........')
+
+@app.route('/user_list')
+def user_list():
+    return Response('........')
 
 if __name__ == '__main__':
     app.run()
